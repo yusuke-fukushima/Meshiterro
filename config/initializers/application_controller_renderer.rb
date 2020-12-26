85105fc871eb42c82c22018eeca58b085de54701
yusuke-fukushima <usk.fshima.1994@gmail.com>
2020-12-26 02:50:15 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '45c15ac1ba72b1596a906acda40c20bc1ba0b3304fc47c6af330ca29019ce3fe6ee0ce5e2f9332489e48cdfa09e88f89c71fee592ebf814a615396885c057904'
