34195cad5be55ee796afdf1cf6d5aaf28a1351d6
yusuke-fukushima <usk.fshima.1994@gmail.com>
2020-12-26 07:39:04 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :post_images, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  attachment :profile_image
 end
